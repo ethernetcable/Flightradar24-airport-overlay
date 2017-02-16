8035e6edde4d4b09dfc987ecd2e6fee5fec794f4
Louis Finlayson <ethernetcable@users.noreply.github.com>
2017-02-16 21:25:55 +0000

    Update fr24-airport-overlay-data.js

--- js/fr24-airport-overlay-data.js
+++ js/fr24-airport-overlay-data.js
@@ -17,78 +17,12 @@ var fr24_airport_overlay_data = [
 		neBoundLng: -87.8683,
 		swBoundLat: 41.9583,
 		swBoundLng: -87.9417,
-		name: "Chicago O\'Hare International Airport",
-		IATA: "ORD",
-		ICAO: "KORD",
-		uploader: "mkyung",
+		name: "San Francisco International Airport",
+		IATA: "SFO",
+		ICAO: "KSFO",
+		uploader: "ethernetcable",
 		email: "",
-		date: 2014,
-		image: "KORD.png"
-	},
-	{
-		neBoundLat: 22.3283,
-		neBoundLng: 113.9467,
-		swBoundLat: 22.29,
-		swBoundLng: 113.881777,
-		name: "Hong Kong International Airport",
-		IATA: "HKG",
-		ICAO: "VHHH",
-		uploader: "mkyung",
-		email: "",
-		date: 2010,
-		image: "VHHH_AIP.png"
-	},
-	{
-		neBoundLat: 35.57667,
-		neBoundLng: 139.82333,
-		swBoundLat: 35.52333,
-		swBoundLng: 139.73667,
-		name: "Haneda Airport",
-		IATA: "HND",
-		ICAO: "RJTT",
-		uploader: "mkyung",
-		email: "",
-		date: 2010,
-		image: "RJTT2.png"
-	},	
-	{
-		neBoundLat: 33.6667,
-		neBoundLng: -84.4017,
-		swBoundLat: 33.61667,
-		swBoundLng: -84.4517,
-		name: "Hartsfield-Jackson Atlanta International Airport",
-		IATA: "ATL",
-		ICAO: "KATL",
-		uploader: "mkyung",
-		email: "",
-		date: 2014,
-		image: "KATL-faa2014.png"
-	},
-	{
-		neBoundLat: 51.152333,
-		neBoundLng: -113.9677,
-		swBoundLat: 51.08816,
-		swBoundLng: -114.0643,
-		name: "Calgary International Airport",
-		IATA: "YYC",
-		ICAO: "CYYC",
-		uploader: "mkyung",
-		email: "",
-		date: 2014,
-		image: "CYYC.png"
-	},	
-	/*
-	{
-		neBoundLat: -33.92273053019009,
-		neBoundLng: 151.20755110740658,
-		swBoundLat: -33.9812189189758,
-		swBoundLng: 151.14664332389827,
-		name: "Sydney Airport",
-		IATA: "SYD",
-		ICAO: "YSSY",
-		uploader: "mkyung",
-		email: "",
-		date: 2014,
-		image: "YSSY.png"
-	},	*/
-	]
+		date: 2017,
+		image: "ksfo_transparent.png"
+	}
+]
